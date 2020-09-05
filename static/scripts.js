0b92ff272387008b1d28df367983ce5143160456
jeremyt <jez_is_the_best@hotmail.com>
2020-09-05 23:48:30 +0100

    fixed projects slider

--- static/scripts.js
+++ static/scripts.js
@@ -1,41 +1,77 @@
 
 
-$('#project-container').scroll(function(e) {
-	alert("Hi");
-	var currentX = document.getElementById('project-container').scrollLeft - document.getElementById('project1').scrollLeft;
-	var cItemMarginR = parseFloat(style.marginRight);
-	var cItemWidth = document.getElementById('project1').offsetWidth + cItemMarginR*2;
+$(document).ready(function() {
+	alert("Hi")
+	$('#project-container').scroll(function() {
+		alert("Hi");
+		
+		// var currentX = document.getElementById('project-container').scrollLeft - document.getElementById('project1').scrollLeft;
+		
+		// var style = document.getElementById('project1').currentStyle || window.getComputedStyle(document.getElementById('project1'));
+		// var cItemMarginR = parseFloat(style.marginRight);
+		// var cItemWidth = document.getElementById('project1').offsetWidth + cItemMarginR*2;
 
-	updateControlActive(currentX, cItemWidth);
+		// updateControlActive(currentX, cItemWidth);
 
+	} );
 } );
+
+
 
 
 function test(){
 	alert("Test alert.")
 }
 
+function isScrolled(){
+		var currentX = document.getElementById('project-container').scrollLeft - document.getElementById('project1').scrollLeft;
+		
+		var style = document.getElementById('project1').currentStyle || window.getComputedStyle(document.getElementById('project1'));
+		var cItemMarginR = parseFloat(style.marginRight);
+		var cItemWidth = document.getElementById('project1').offsetWidth + cItemMarginR*2;
+
+		updateControlActive(currentX, cItemWidth);
+}
 
 
 function updateControlActive(currentX, itemWidth){
-	alert("currentX"+currentX+" low:"+itemWidth-(itemWidth/10) + " high:"+itemWidth+(itemWidth/10));
-	if (currentX > itemWidth-(itemWidth/10) && currentX < itemWidth+(itemWidth/10)) {
-		document.getElementById('project2').add("active");
+	// Function to get index of item that was scrolled to
+	function getProjIndex(currentX, itemWidth, allDots){
+		for (let i=0; i<allDots.length; i++){
+			if (currentX == 0) {
+				return i;
+			}
+			if ( currentX > (i*itemWidth)-(itemWidth/10) && currentX < (i*itemWidth)+(itemWidth/10) ){
+				return i;
+			}
+		}
 	}
 
-}
-function show_x(){
-	var style = document.getElementById('project1').currentStyle || window.getComputedStyle(document.getElementById('project1'));
+	var allDots = document.getElementsByClassName('dot');
+	
+	// Make old dot inactive
+	for (let i = 0; i < allDots.length; i++){
+		if (allDots[i].classList.contains('active')){
+			allDots[i].classList.remove('active');
+			break;
+		}
+	}
 
-	var containerWidth = document.getElementById('project-container').scrollWidth;
+	// Make dot active
+	var id = getProjIndex(currentX, itemWidth, allDots);
+	allDots[id].classList.add('active');
 
-
-
-	var numCarouselItems = $('.carousel-item').length;
-	var x = parseInt(document.getElementById('project-container').scrollLeft - document.getElementById('project1').scrollLeft);
-	alert("Container-width:"+containerWidth +"scrollLeft-position:"+x + "num-items:"+numCarouselItems + " item-width:" + cItemWidth);
-
-
+	// // Make carousel arrows hidden on either end
+	// if (id == 0){
+	// 	document.getElementsByClassName('c-a-left')[0].style.visibility = "hidden";
+	// }
+	// else if (id == allDots.length-1){
+	// 	document.getElementsByClassName('c-a-right')[0].style.visibility = "hidden";
+	// }
+	// else if (id != 0 || id != allDots.length-1){
+	// 	document.getElementsByClassName('carousel-arrows')[0].style.visibility = "visible";
+	// 	document.getElementsByClassName('carousel-arrows')[1].style.visibility = "visible";
+	// }
 
 }
 
@@ -51,18 +87,34 @@ function scrollToAnchorHorizontal(anchor){
 
 
 
+function scroll_Next(x){
+	var allDots = document.getElementsByClassName('dot');
+	var id = 0;
 
+	for (let i = 0; i < allDots.length; i++){
+		if (allDots[i].classList.contains('active')){
+			if (i+x < 0){
+				id = allDots.length-1;
+			}
+			else if (i+x == allDots.length){
+				id = 0;
+			}
+			else {
+				id = i + x;
+			}
+			break;
+		}
+	}
+	var project = 'project'+id;
+	scrollToAnchorHorizontal(project);
+}
 
 function scroll_Right(){
-	alert("Right");
-	var container = document.getElementById('#project-container');
+	var container = document.getElementById('project-container');
 	container.scrollLeft += 500;
-	alert("Right finished");
 }
 
 function scroll_Left(){
-	alert("Left");
-	var container = document.getElementById('#project-container');
+	var container = document.getElementById('project-container');
 	container.scrollLeft -= 500;
-	alert("Left finished");
 }
